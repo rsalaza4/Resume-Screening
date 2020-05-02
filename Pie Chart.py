bae0f489a597560e10bd43affc6e3c4e9639731e
Roberto Salazar <56412788+rsalaza4@users.noreply.github.com>
2020-05-02 13:12:59 -0400

    Add files via upload

---
+++ Pie Chart.py
@@ -0,0 +1,9 @@
+# Creat pie chart visualization
+pie = plt.figure(figsize=(10,10))
+plt.pie(summary['score'], labels=summary.index, explode = (0.1,0,0,0,0,0), autopct='%1.0f%%',shadow=True,startangle=90)
+plt.title('Industrial Engineering Candidate - Resume Decomposition by Areas')
+plt.axis('equal')
+plt.show()
+
+# Save pie chart as a .png file
+pie.savefig('resume_screening_results.png')
